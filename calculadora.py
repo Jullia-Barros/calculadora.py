e68aeb85c040fa1d60dc141ac3bd9da6c1b35d6a
Jullia Barros <94147889+Jullia-Barros@users.noreply.github.com>
2022-09-05 13:02:10 -0300

    Primeiro commit. Sujeito a alterações

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+print ("################################")
+print("Hello word!!")
+print ("################################")
+
+#Essa é uma calculadora em python
+
+numero1= input ("Digite um primeiro número: ")
+numero1= int(numero1)
+
+numero2= input ("Digite um segundo número: ")
+numero2= int(numero2)
+
+print("O resultado da soma é:")
+print(numero1+ numero2)
+
+print("O resultado da subtração é: ")
+print(numero1 - numero2)
+
+print("O resultado da multiplicação é: ")
+print(numero1* numero2)
+
+print("O resultado da divisão é: ")
+print(numero1/ numero2)
+
+print("Obrigada por sua participação!")
+
+
+
+
+
+
+
+
